bcb340476723037773067de1c3df66d867657f1a
George <gchriscarter@gmail.com>
2021-08-16 22:21:59 -0400

    get svelte and ember to display data on chart, need to add auto refresh

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,6 @@
-/* global Chart */
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
-// import Angular from "../views/Angular.vue";
-// import Ember from "../views/Ember.vue";
-// import React from "../views/React.vue";
-// import Svelte from "../views/Svelte.vue";
-// import VueJS from "../views/VueJS.vue";
 
 Vue.use(VueRouter);
 
@@ -57,55 +50,5 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
-
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Stars", "Watchers", "Forks"],
-    datasets: [
-      {
-        label: "Framework Popularity",
-        data: [12, 19, 3],
-        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
-        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
-
-// var ctx = document.getElementById("myChart").getContext("2d");
-// var Chart = new Chart(ctx, {
-//   type: "bar",
-//   data: {
-//     labels: ["Stars", "Watchers", "Forks"],
-//     datasets: [
-//       {
-//         label: "Popularity",
-//         data: [12, 5, 8],
-//         // data: [this.repo.subscriber_count, this.repo.watchers, this.repo.forks],
-
-//         backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
-//         borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
-//         borderWidth: 1,
-//       },
-//     ],
-//   },
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true,
-//       },
-//     },
-//   },
-// });
 
 export default router;
--- src/views/Ember.vue
+++ src/views/Ember.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="ember">
-    <h3>{{ this.repo.description }}</h3>
-    <div>Stars - {{ this.repo.subscribers_count }}</div>
-    <div>Watchers - {{ this.repo.watchers }}</div>
-    <div>Forks - {{ this.repo.forks }}</div>
+    <!-- <h3>{{ this.repo.description }}</h3>
+    <div>Stars - {{ this.stars }}</div>
+    <div>Watchers - {{ this.watchers }}</div>
+    <div>Forks - {{ this.forks }}</div> -->
   </div>
 </template>
 
@@ -22,7 +22,35 @@ export default {
       .get("https://api.github.com/repos/emberjs/ember.js")
       .then((response) => {
         this.repo = response.data;
+        console.log(response.data.subscribers_count, response.data.watchers, response.data.forks, response.data);
+        this.forks = response.data.forks;
+        this.watchers = response.data.watchers;
+        this.stars = response.data.subscribers_count;
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myChart = new Chart(ctx, {
+          type: "bar",
+          data: {
+            labels: ["Stars", "Watchers", "Forks"],
+            datasets: [
+              {
+                label: "EmberJS",
+                data: [this.stars, this.watchers, this.forks],
+                backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)"],
+                borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)"],
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        });
       })
+
       .catch(function (error) {
         // handle error
         console.log(error);
